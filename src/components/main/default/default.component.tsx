c1670d7f0ac8ee7c234390eea677b8ba8dd87f8a
Daniel Chong <danchong625@gmail.com>
2020-01-28 21:23:50 -0800

    initializes default view component

---
+++ src/components/main/default/default.component.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Button } from 'primereact/button';
+
+import './default.component.css';
+
+interface IProps {
+  clickHandler: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export function DefaultView(props: IProps) {
+  return (
+    <div id="default-view">
+      <div id="description">
+        <p>As interplanetary travel becomes mainstream, we're excited to offer rocket owners comprehensive coverage options to let them fly through space worry-free.</p>
+      </div>
+      <div id="default-button-row">
+        <Button label="Get a Quote" className="p-button-raised p-button-rounded" onClick={props.clickHandler} />
+      </div>
+    </div>
+  )
+}
