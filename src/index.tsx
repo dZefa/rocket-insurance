e6bc70d51aab689f459b22df2ab420e6f610f839
Daniel Chong <danchong625@gmail.com>
2020-01-28 21:22:59 -0800

    adds primereact and primeicon imports

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import { render } from 'react-dom';
 
 import MainView from './components/main/main.component';
 
+import 'primereact/resources/themes/nova-dark/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 import './index.css';
 
 render(
