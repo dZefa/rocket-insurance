39b8ab7829d4a14ab4412e5e14b01ab4dae18923
Daniel Chong <danchong625@gmail.com>
2020-01-28 19:54:34 -0800

    initializes webpack config for dev and prod

---
+++ webpack.common.js
@@ -0,0 +1,60 @@
+const webpack = require('webpack');
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ExtractCSSPlugin = require('mini-css-extract-plugin');
+
+const APP_DIR = path.resolve(__dirname, './src/index.tsx');
+const BUILD_DIR = path.resolve(__dirname, './build');
+const TEMPLATE_DIR = path.resolve(__dirname, './src/template.index.html');
+
+module.exports = {
+  entry: {
+    app: APP_DIR,
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-syntax-dynamic-import', '@babel/plugin-transform-modules-commonjs'],
+            },
+          },
+          {
+            loader: 'ts-loader',
+          }
+        ]
+      },
+      {
+        test: /.css$/,
+        use: [
+          ExtractCSSPlugin.loader,
+          'css-loader',
+          'style-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new ExtractCSSPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    }),
+    new HTMLPlugin({
+      template: TEMPLATE_DIR,
+      inject: true
+    })
+  ]
+};
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const merge = require('webpack-merge');
+const path = require('path');
+
+const common = require('./webpack.common');
+
+switch (process.env.NODE_ENV) {
+  case 'production': {
+    module.exports = merge(common, {
+      mode: 'production',
+      optimization: {
+        splitChunks: {
+          cacheGroups: {
+            commons: {
+              name: 'commons',
+              minChunks: 2
+            }
+          }
+        }
+      }
+    });
+
+    break;
+  }
+
+  default: {
+    module.exports = merge(common, {
+      mode: 'development',
+      devServer: {
+        open: true,
+        port: 0824
+      }
+    });
+  }
+};
