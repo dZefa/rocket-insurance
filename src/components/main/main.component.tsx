2f98cd315d3ae8f41d4865cf22d2b7261b2a8492
Daniel Chong <danchong625@gmail.com>
2020-01-28 21:34:48 -0800

    implements default viewport view with click handler to open rating dialog

--- src/components/main/main.component.tsx
+++ src/components/main/main.component.tsx
@@ -2,19 +2,31 @@ import * as React from 'react';
 
 import './main.component.css';
 
+import { DefaultView } from './default/default.component';
+
 interface IProps {
 
 }
 
 interface IState {
-
+  showForm: boolean;
 }
 
 class MainView extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      showForm: false,
+    };
+
+    this.ratingClickHandler = this.ratingClickHandler.bind(this);
+  }
+
+  public ratingClickHandler(e: React.MouseEvent<HTMLButtonElement>): void {
+    e.preventDefault();
+
+    this.setState({ showForm: true });
   }
 
   render() {
@@ -23,7 +35,8 @@ class MainView extends React.Component<IProps, IState> {
         <header id="header">
           <h1>Rocket Insurance</h1>
         </header>
-        <div id="dynamic-view">
+        <div id="viewport">
+          <DefaultView clickHandler={this.ratingClickHandler}></DefaultView>
         </div>
       </div>
     )
