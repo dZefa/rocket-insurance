c186f2d642fc510961e81190ecbb424abc11013f
Daniel Chong <danchong625@gmail.com>
2020-01-30 17:41:45 -0800

    initializes loading component

---
+++ src/components/loading/loading.component.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { ProgressSpinner } from 'primereact/progressspinner';
+
+import './loading.component.css';
+
+interface IProps {
+
+}
+
+export function LoadingView(props: IProps) {
+  return (
+    <div id="loading">
+      <ProgressSpinner></ProgressSpinner>
+      <div id="loading-description">
+        <h1>Jupiter recognize!</h1>
+        <h3>We're getting you the best quote in the universe!</h3>
+      </div>
+    </div>
+  )
+}
